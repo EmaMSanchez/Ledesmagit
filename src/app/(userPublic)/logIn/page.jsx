9252df132b02984aadb72a6a79588ba104a6fb91
EmaMSanchez <emamsanchez07@gmail.com>
2023-10-16 21:08:33 -0300

    Correccion Estados User y push

--- src/app/(userPublic)/logIn/page.jsx
+++ src/app/(userPublic)/logIn/page.jsx
@@ -1,9 +1,41 @@
+"use client"
+
+import { useUserContext } from '@/context/UserContext'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const LogIn = () => {
+
+  const navigate = useRouter()
+
+   const {user, setUser} = useUserContext()
+
+   const handleState = (estado) =>{
+    setUser(estado);
+    if(estado == "supUser"){
+     navigate.push('/superUser')
+    }else if(estado == "maestro"){
+     navigate.push('/obrasUser')
+    }else if (estado == "user") {
+     navigate.push('/inversorUser')
+    }else{
+      
+      navigate.push('/')
+    }
+  }
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h1>LogIn</h1>
+    <div className='container flex justify-center gap-6'>
+        
+          <button onClick={()=>handleState("user")} className='rounded-full bg-black text-white w-28'>User</button>
+          <button onClick={()=>handleState("maestro")} className='rounded-full bg-black text-white w-28'>Maestro</button>
+          <button onClick={()=>handleState("supUser")} className='rounded-full bg-black text-white w-28'>Super User</button>
+          <button onClick={()=>handleState(null)} className='rounded-full bg-black text-white w-28'>Log Out</button>
+       
+    </div>
    </main>
   )
 }
--- src/app/obrasUser/page.jsx
+++ src/app/obrasUser/page.jsx
@@ -1,8 +1,59 @@
+"use client"
+import Buscador from '@/components/Buscador';
+import CardDestacado from '@/components/CardDestacado';
+import CardSubProyectos from '@/components/CardSubProyectos';
+import ImagenPrueba from '@/components/ImagenPrueba';
+import { useUserContext } from '@/context/UserContext'
 import React from 'react'
 
+
 const page = () => {
+  const {user,setUser} = useUserContext();
+
   return (
-    <div>page</div>
+    <main className="flex flex-col items-center p-16">
+     <div className="container border border-gray-400 w-[1000px] bg-gray-100 rounded-[30px] ">
+      <div className="flex justify-between pl-9 overflow-visible">
+       <h1 className="text-[64px] mt-10 inline-block">Titulo H1 Desarrollos Maestro Mayor de Obras</h1>
+      </div>
+      <div className="flex justify-between pl-9 gap-7 ">
+       <h2 className="text-[36px] text-gray-400/75 pt inline-block">Sub titulo H2 descripcion y atencion User Default</h2>
+      <div className="mb-16 mr-9 inline-block mt-[-50px]">
+      <ImagenPrueba/>
+      </div>
+      </div>
+      <Buscador/>
+     </div>
+     <div className="container w-[1000px]">
+      <div className=" mt-16 text-left pl-[5px] flex justify-between">
+        <h1 className="text-[24px] h-[28px] font-light"><span className="text-[32px] h-[38px] text-black font-bold mr-3">Proyectos</span> Lo màs destacado</h1>
+        <div className="flex items-center">
+    <p className="text-[14px] font-normal mr-2">LO MAS DESTACADO</p>
+    <img src="/pngwing.com.png" className="w-[7.7px]" alt="Icono" />
+  </div>
+       </div>
+       <div className="container flex justify-between mt-14 gap-12 ">
+            <CardDestacado/>
+            <CardDestacado/>
+            <CardDestacado/>
+       </div>
+       <div className="mt-16 text-left pl-[5px] flex justify-between items-center">
+  <h1 className="text-[24px] h-[28px] font-light">
+    <span className="text-[32px] h-[38px] text-black font-bold mr-3">Sub Proyectos </span>  
+    Últimos disponibles
+  </h1>
+  <div className="flex items-center">
+    <p className="text-[14px] font-normal mr-2">ÚLTIMOS DISPONIBLES</p>
+    <img src="/pngwing.com.png" className="w-[7.7px]" alt="Icono" />
+  </div>
+</div>
+       <div className="container flex justify-between mt-14 gap-12 ">
+            <CardSubProyectos/>
+            <CardSubProyectos/>
+            <CardSubProyectos/>
+       </div>
+     </div>
+    </main>
   )
 }
 
