49b80ddf490b45d98ddd0891afae28eab854adff
EmaMSanchez <emamsanchez07@gmail.com>
2023-10-23 21:23:25 -0300

    Etiqueta card y alerta dinamica log

--- src/app/(userPublic)/page.jsx
+++ src/app/(userPublic)/page.jsx
@@ -1,34 +1,53 @@
-import BotonesPaginaAvanzado from '@/components/BotonesPaginaAvanzado'
-import CardDestacado from '@/components/CardDestacado'
+import Buscador from "@/components/Buscador";
+import CardDestacado from "@/components/CardDestacado";
+import CardSubProyectos from "@/components/CardSubProyectos";
+import ImagenPrueba from "@/components/ImagenPrueba";
 
-import React from 'react'
-
-const SearchPage = () => {
-  return (
-    <main className='flex flex-col items-center p-24'>
-    <form>
-      <div className='flex justify-center mb-8'>
-       <input type="text" placeholder="Busca por nombre del proyecto o sub proyecto" className="bg-gray-200 rounded-full h-10 w-[602px] pl-12 placeholderImage " />
+export default function Home() {
+ return (
+    <main className="flex flex-col items-center p-16">
+     <div className="container border border-gray-400 w-[1000px] bg-gray-100 rounded-[30px] ">
+      <div className="flex justify-between pl-9 overflow-visible">
+       <h1 className="text-[64px] mt-10 inline-block">Titulo H1 Desarrollos</h1>
       </div>
-    </form>
-    <h1 className='font-semibold text-[32px]'>¿Qué estas buscando?</h1>
-    <BotonesPaginaAvanzado/>
-    <div className='bg-gray-200 rounded-[30px] w-[968px] h-[352px] mt-24'>
-    </div>
-    <div>
-    <div className="container flex justify-between mt-24 gap-12 ">
-            <CardDestacado/>
-            <CardDestacado/>
-            <CardDestacado/>
+      <div className="flex justify-between pl-9 gap-7 ">
+       <h2 className="text-[36px] text-gray-400/75 pt inline-block">Sub titulo H2 descripcion y atencion User Default</h2>
+      <div className="mb-16 mr-9 inline-block mt-[-50px]">
+      <ImagenPrueba/>
+      </div>
+      </div>
+      <Buscador ruta={"/"}/>
+     </div>
+     <div className="container w-[1000px]">
+      <div className=" mt-16 text-left pl-[5px] flex justify-between">
+        <h1 className="text-[24px] h-[28px] font-light"><span className="text-[32px] h-[38px] text-black font-bold mr-3">Proyectos</span> Lo màs destacado</h1>
+        <div className="flex items-center mb-[-25px]">
+    <p className="text-[14px] font-normal mr-2 ">LO MAS DESTACADO</p>
+    <img src="/pngwing.com.png" className="w-[7.7px]" alt="Icono" />
+  </div>
        </div>
        <div className="container flex justify-between mt-14 gap-12 ">
             <CardDestacado/>
             <CardDestacado/>
             <CardDestacado/>
        </div>
-    </div>
+       <div className="mt-16 text-left pl-[5px] flex justify-between items-center">
+  <h1 className="text-[24px] h-[28px] font-light">
+    <span className="text-[32px] h-[38px] text-black font-bold mr-3">Sub Proyectos </span>  
+    Últimos disponibles
+  </h1>
+  <div className="flex items-center mb-[-25px]">
+    <p className="text-[14px] font-normal mr-2">ÚLTIMOS DISPONIBLES</p>
+    <img src="/pngwing.com.png" className="w-[7.7px]" alt="Icono" />
+  </div>
+</div>
+       <div className="container flex justify-between mt-14 gap-12 ">
+            <CardSubProyectos/>
+            <CardSubProyectos/>
+            <CardSubProyectos/>
+       </div>
+     </div>
     </main>
   )
 }
 
-export default SearchPage
--- src/components/CardSubProyectos.jsx
+++ src/components/CardSubProyectos.jsx
@@ -1,14 +1,25 @@
+"use client"
+import { useUserContext } from '@/context/UserContext';
 import React from 'react'
+
+
 
 const CardSubProyectos = () => {
   const divStyle = {
     backgroundImage: `url("/images.jpg")`,
     backgroundSize: 'cover', // Ajusta la imagen al tamaño del contenedor
   };
-    
+  const {user, setUser} = useUserContext();
+ 
   return (
     <div className='w-[360px] h-[470px] bg-gray-100 rounded-[14px]'>
-      <div className='rounded-t-[14px] overflow-hidden h-32 bg-gray-200' style={divStyle}>
+      <div className='rounded-t-[14px] overflow-hidden h-32 bg-gray-200' style={divStyle}> {/*Modificar por Image */}
+       {
+          user !== null && 
+          <div className='flex justify-end'>
+          <div className='bg-yellow-300 rounded-bl-[14px] rounded-tr-[14px] text-center w-[131px] h-[24px]'>NOVEDADES</div>
+          </div>
+       }
       </div>
       <div className='container ml-6 mt-5'>
         <p className='font-medium mt-[2px]'>Categoría</p>
