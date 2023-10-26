d23236bb8432dad81f64cb8ed0f1baf3ea92d005
miribarren <miribarren@beskar.net>
2023-10-26 16:48:48 -0300

    Pruebo una funcion onCall

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 "use client"
 
 import Link from 'next/link';
-import Image from "next/image";
 import React from 'react';
 import { useUserContext } from '@/context/UserContext';
 import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import * as functions from 'firebase/functions';
+import { functionsApp } from '@/services/firebase.service';
 
 const Cabecero = () => {
 
@@ -14,6 +14,47 @@ const Cabecero = () => {
 
   const isActive = (ruta) => {
     return active === ruta || active.startsWith(ruta + "/");
+  }
+
+  const handleClick = async () => {
+
+    /*
+    {
+      id?: string;
+      estado?: Estado;
+      tipo: Tipo;
+      nombre: string;
+      trending?: boolean;
+      precio: number;
+      proyectoPadre?: string; //proyecto id
+      etiquetas?: string[]; // 'ultimos-disponibles'
+      usuarios?: {
+        inversor?: string[]; // users id list
+        agente?: string[]; // users id list
+        arquitecto?: string[];
+      };
+      subproyectos?: string[];
+      informacion: Informacion;
+      notificaciones?: string[];
+    }
+
+
+    */
+    let myFunction = functions.httpsCallable(functionsApp, 'proyects-create');
+
+    let call = await myFunction({
+      tipo: 'proyecto',
+      nombre: 'Proyecto de prueba',
+      precio: 10000,
+      informacion: {
+        direccion: 'Chaco 1670',
+        ciudad: 'Mar del Plata',
+        provincia: 'Buenos Aires'
+      }
+    });
+
+    console.log(`Call result: ${call}`);
+
   }
 
   /*
@@ -29,6 +70,7 @@ const Cabecero = () => {
           <h1 className='text-xl font-sans'><span className='font-bold'>H.Ledesma</span> Desarrollos</h1>
         </div>
         <div className="flex items-center">
+          <button onClick={handleClick}>call funct</button>
           <Link href="/login" className={` hover:bg-blue-500 font-normal rounded-full hover:text-white py-1 px-6 border-2 hover:border-transparent text-xs ${isActive("/login") ? "bg-blue-500 text-white border-blue-500" : "bg-transparent text-black border-black"}`}>
             {user ? 'Mis inversiones' : 'Ingresar'}
           </Link>
--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFunctions } from "firebase/functions"
 import { getAuth } from "firebase/auth";
 
 
@@ -15,4 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const functionsApp = getFunctions(app);
 export const auth = getAuth(app);
